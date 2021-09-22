fe250d2ab18c039fbadb937a04b1f67c9de32886
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-09-22 19:21:45 +0530

    facilities list api call

--- src/actions/facilityActions.js
+++ src/actions/facilityActions.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 import {
-    FACILITY_LIST_REQUEST,FACILITY_LIST_SUCCESS,FACILITY_LIST_FAIL
+    FACILITY_LIST_REQUEST,FACILITY_LIST_SUCCESS,FACILITY_LIST_FAIL,
+    FACILITY_ACTIVE_REQUEST,FACILITY_ACTIVE_SUCCESS,FACILITY_ACTIVE_FAIL,
+    FACILITY_RECOVERED_REQUEST,FACILITY_RECOVERED_SUCCESS,FACILITY_RECOVERED_FAIL,
+    FACILITY_DEATHS_REQUEST,FACILITY_DEATHS_SUCCESS,FACILITY_DEATHS_FAIL,
+    FACILITY_BEDS_REQUEST,FACILITY_BEDS_SUCCESS,FACILITY_BEDS_FAIL
 } from '../constants/facilityConstants';
 
-export const listFacilities = () => async (dispatch) => {
+export const listFacilities = () => async (dispatch,getState) => {
     try {
         dispatch({type: FACILITY_LIST_REQUEST});
 
-        const { data } = await axios.get(`/moh/facilities`);
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+
+        const { data } = await axios.get(`/moh/facilities`,config);
 
         dispatch({
             type: FACILITY_LIST_SUCCESS,
@@ -17,6 +30,126 @@ export const listFacilities = () => async (dispatch) => {
     catch (error) {
         dispatch({
             type: FACILITY_LIST_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message :
+                error.message
+        })
+    }
+};
+
+export const listFacilitiesActive = () => async (dispatch,getState) => {
+    try {
+        dispatch({type: FACILITY_ACTIVE_REQUEST});
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+
+        const { data } = await axios.get(`/moh/facilities/active`,config);
+
+        dispatch({
+            type: FACILITY_ACTIVE_SUCCESS,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: FACILITY_ACTIVE_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message :
+                error.message
+        })
+    }
+};
+
+export const listFacilitiesRecovered = () => async (dispatch,getState) => {
+    try {
+        dispatch({type: FACILITY_RECOVERED_REQUEST});
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+
+        const { data } = await axios.get(`/moh/facilities/recovered`,config);
+
+        dispatch({
+            type: FACILITY_RECOVERED_SUCCESS,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: FACILITY_RECOVERED_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message :
+                error.message
+        })
+    }
+};
+
+export const listFacilitiesDeaths = () => async (dispatch,getState) => {
+    try {
+        dispatch({type: FACILITY_DEATHS_REQUEST});
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+
+        const { data } = await axios.get(`/moh/facilities/deaths`,config);
+
+        dispatch({
+            type: FACILITY_DEATHS_SUCCESS,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: FACILITY_DEATHS_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message :
+                error.message
+        })
+    }
+};
+
+export const listFacilitiesBeds = () => async (dispatch,getState) => {
+    try {
+        dispatch({type: FACILITY_BEDS_REQUEST});
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+
+        const { data } = await axios.get(`/moh/facilities/beds`,config);
+
+        dispatch({
+            type: FACILITY_BEDS_SUCCESS,
+            payload: data
+        })
+    }
+    catch (error) {
+        dispatch({
+            type: FACILITY_BEDS_FAIL,
             payload: error.response && error.response.data.message ?
                 error.response.data.message :
                 error.message
--- src/reducers/facilityReducers.js
+++ src/reducers/facilityReducers.js
@@ -1,5 +1,9 @@
 import {
-    FACILITY_LIST_REQUEST,FACILITY_LIST_SUCCESS,FACILITY_LIST_FAIL
+    FACILITY_LIST_REQUEST,FACILITY_LIST_SUCCESS,FACILITY_LIST_FAIL,
+    FACILITY_ACTIVE_REQUEST,FACILITY_ACTIVE_SUCCESS,FACILITY_ACTIVE_FAIL,
+    FACILITY_RECOVERED_REQUEST,FACILITY_RECOVERED_SUCCESS,FACILITY_RECOVERED_FAIL,
+    FACILITY_DEATHS_REQUEST,FACILITY_DEATHS_SUCCESS,FACILITY_DEATHS_FAIL,
+    FACILITY_BEDS_REQUEST,FACILITY_BEDS_SUCCESS,FACILITY_BEDS_FAIL
 } from '../constants/facilityConstants';
 
 export const facilityListReducer = (state = { facilitiesList: {results:[],message:''} }, action) => {
@@ -9,6 +13,58 @@ export const facilityListReducer = (state = { facilitiesList: {results:[],messag
         case FACILITY_LIST_SUCCESS:
             return { loading: false, facilitiesList: action.payload};
         case FACILITY_LIST_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const facilitiesActiveReducer = (state = { facilitiesActiveList: {results:[],message:''} }, action) => {
+    switch (action.type) {
+        case FACILITY_ACTIVE_REQUEST:
+            return { loading: true, facilitiesActiveList: {results:[],message:''} };
+        case FACILITY_ACTIVE_SUCCESS:
+            return { loading: false, facilitiesActiveList: action.payload};
+        case FACILITY_ACTIVE_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const facilitiesRecoveredReducer = (state = { facilitiesRecoveredList: {results:[],message:''} }, action) => {
+    switch (action.type) {
+        case FACILITY_RECOVERED_REQUEST:
+            return { loading: true, facilitiesRecoveredList: {results:[],message:''} };
+        case FACILITY_RECOVERED_SUCCESS:
+            return { loading: false, facilitiesRecoveredList: action.payload};
+        case FACILITY_RECOVERED_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const facilitiesDeathsReducer = (state = { facilitiesDeathsList: {results:[],message:''} }, action) => {
+    switch (action.type) {
+        case FACILITY_DEATHS_REQUEST:
+            return { loading: true, facilitiesDeathsList: {results:[],message:''} };
+        case FACILITY_DEATHS_SUCCESS:
+            return { loading: false, facilitiesDeathsList: action.payload};
+        case FACILITY_DEATHS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const facilitiesBedsReducer = (state = { facilitiesBedsList: {results:[],message:''} }, action) => {
+    switch (action.type) {
+        case FACILITY_BEDS_REQUEST:
+            return { loading: true, facilitiesBedsList: {results:[],message:''} };
+        case FACILITY_BEDS_SUCCESS:
+            return { loading: false, facilitiesBedsList: action.payload};
+        case FACILITY_BEDS_FAIL:
             return { loading: false, error: action.payload };
         default:
             return state;
